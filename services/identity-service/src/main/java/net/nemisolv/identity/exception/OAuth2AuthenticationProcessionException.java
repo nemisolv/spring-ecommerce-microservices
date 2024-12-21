95246c14b6dd6d783a74b124adfcbcc3883df77a
nemisolv <nemisolv@gmail.com>
2024-12-21 10:40:13 +0700

    config CORS for gateway

---
+++ services/identity-service/src/main/java/net/nemisolv/identity/exception/OAuth2AuthenticationProcessionException.java
@@ -0,0 +1,16 @@
+package net.nemisolv.lib.core.exception;
+
+
+import javax.naming.AuthenticationException;
+
+public class OAuth2AuthenticationProcessionException extends AuthenticationException {
+public OAuth2AuthenticationProcessionException(String msg) {
+    super(msg);
+}
+
+public OAuth2AuthenticationProcessionException(String msg,Throwable t) {super(msg);}
+
+
+
+
+}
---
+++ services/identity-service/src/main/java/net/nemisolv/identity/payload/auth/ResendEmailConfirmationRequest.java
@@ -0,0 +1,2 @@
+package net.nemisolv.identity.payload.auth;public record ResendEmailConfirmationRequest() {
+}
